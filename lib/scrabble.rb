6e0e20efdf26effde750477ae2f1f6bbb67559cd
John Rykken and Nicko Dela Cruz <nicko@>
2018-02-06 10:46:18 -0800

    wrote and failed/passed 3 tests. assigned key values for every letter

---
+++ lib/scrabble.rb
@@ -0,0 +1,43 @@
+class String
+  def a_value
+    score = "1"
+    score
+  end
+
+  def d_value
+    score = "2"
+    score
+  end
+
+  def letter_value
+    letter = Hash.new()
+    letter[:"a"] = 1
+    letter[:"e"] = 1
+    letter[:"i"] = 1
+    letter[:"o"] = 1
+    letter[:"u"] = 1
+    letter[:"l"] = 1
+    letter[:"n"] = 1
+    letter[:"r"] = 1
+    letter[:"s"] = 1
+    letter[:"t"] = 1
+    letter[:"d"] = 2
+    letter[:"g"] = 2
+    letter[:"b"] = 3
+    letter[:"c"] = 3
+    letter[:"m"] = 3
+    letter[:"p"] = 3
+    letter[:"f"] = 4
+    letter[:"h"] = 4
+    letter[:"v"] = 4
+    letter[:"w"] = 4
+    letter[:"y"] = 4
+    letter[:"k"] = 5
+    letter[:"j"] = 8
+    letter[:"x"] = 8
+    letter[:"q"] = 10
+    letter[:"z"] = 10
+
+  end
+
+end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,3 +1,22 @@
 require('rspec')
 require('scrabble')
 require('pry')
+
+
+describe('#a_value') do
+    it("returns a scrabble score for a letter") do
+    expect("a".a_value()).to(eq("1"))
+  end
+end
+
+describe('#d_value') do
+    it("returns a scrabble score for a letter") do
+    expect("d".d_value()).to(eq("2"))
+  end
+end
+
+describe('#letter_value') do
+    it("returns a scrabble score for a letter") do
+    expect("z".letter_value()).to(eq(10))
+  end
+end
