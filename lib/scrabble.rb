dafcd49eef4aabea73eca91599d9f66d3d7a39a4
John Rykken and Nicko Dela Cruz <nicko@>
2018-02-06 12:05:02 -0800

    Write method, each loop, test scrabble method.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,43 +1,24 @@
-class String
-  def a_value
-    score = "1"
+# This entire bit of code is similar to javascript backend code.
+
+require('pry')
+
+class Word
+  # this bit of code is like a javascript constructor the @word=word is like write this.name = name
+  def initialize(word)
+    @word = word
+  end
+
+  def scrabble
+
+    points = {"a" => 1, "e" => 1, "i" =>1, "o" =>1, "u" =>1, "l" =>1, "n" =>1, "r" =>1, "s" =>1, "t" =>1, "d" =>2, "g" =>2, "b" =>3, "c" =>3, "m" =>3, "p" =>3, "f" =>4, "h" =>4, "v" =>4, "w" =>4, "y" =>4, "k" =>5, "j" =>8, "x" =>8, "q" =>10, "z" =>10}
+
+    score = 0
+    letter_array = @word.split("")
+    letter_array.each do |letter|
+      temp_score = points.fetch(letter)
+      score += temp_score
+    end
+    binding.pry
     score
   end
-
-  def d_value
-    score = "2"
-    score
-  end
-
-  def letter_value
-    letter = Hash.new()
-    letter[:"a"] = 1
-    letter[:"e"] = 1
-    letter[:"i"] = 1
-    letter[:"o"] = 1
-    letter[:"u"] = 1
-    letter[:"l"] = 1
-    letter[:"n"] = 1
-    letter[:"r"] = 1
-    letter[:"s"] = 1
-    letter[:"t"] = 1
-    letter[:"d"] = 2
-    letter[:"g"] = 2
-    letter[:"b"] = 3
-    letter[:"c"] = 3
-    letter[:"m"] = 3
-    letter[:"p"] = 3
-    letter[:"f"] = 4
-    letter[:"h"] = 4
-    letter[:"v"] = 4
-    letter[:"w"] = 4
-    letter[:"y"] = 4
-    letter[:"k"] = 5
-    letter[:"j"] = 8
-    letter[:"x"] = 8
-    letter[:"q"] = 10
-    letter[:"z"] = 10
-
-  end
-
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,21 +2,13 @@ require('rspec')
 require('scrabble')
 require('pry')
 
+# this code is somewhat comparable to javascript front end code
 
-describe('#a_value') do
-    it("returns a scrabble score for a letter") do
-    expect("a".a_value()).to(eq("1"))
-  end
-end
-
-describe('#d_value') do
-    it("returns a scrabble score for a letter") do
-    expect("d".d_value()).to(eq("2"))
-  end
-end
-
-describe('#letter_value') do
-    it("returns a scrabble score for a letter") do
-    expect("z".letter_value()).to(eq(10))
+describe ('word') do
+  describe('#scrabble') do
+    word = Word.new("hello")
+      it("returns a scrabble score for a letter") do
+      expect(word.scrabble()).to(eq(8))
+    end
   end
 end
